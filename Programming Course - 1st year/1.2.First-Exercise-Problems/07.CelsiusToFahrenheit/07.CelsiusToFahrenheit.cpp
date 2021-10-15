702549015f56a7ae8dc38a3e1344c360741513db
Ati Vassileva <ativassileva@gmail.com>
2021-10-15 21:08:56 +0300

    Solved 7th problem

---
+++ Programming Course - 1st year/1.2.First-Exercise-Problems/07.CelsiusToFahrenheit/07.CelsiusToFahrenheit.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	double temperatureInCelsius;
+    cout << "Enter temperature in Celsius: ";
+    cin >> temperatureInCelsius;
+
+    // T(f) = (T(c) × 9/5) + 32
+    double celsiusToFahrenheit = (temperatureInCelsius * 9 / 5) + 32;
+
+    cout << temperatureInCelsius << " C = "
+	<< celsiusToFahrenheit << " F" << endl;
+}
