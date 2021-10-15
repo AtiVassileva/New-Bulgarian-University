dae65a856317918d6697ec83478b8dfdfd0970ad
Ati Vassileva <ativassileva@gmail.com>
2021-10-15 21:20:51 +0300

    Solved 8th problem

---
+++ Programming Course - 1st year/1.2.First-Exercise-Problems/08.ChocolateCake/08.ChocolateCake.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    double chocolateKilos, cakeKilos;
+    double chocolatePrice, cakePrice;
+
+    cout << "Chocolate kilograms: ";
+    cin >> chocolateKilos;
+
+    cout << "Cake kilograms: ";
+    cin >> cakeKilos;
+
+    cout << "Chocolate price: ";
+    cin >> chocolatePrice;
+
+    cout << "Cake price: ";
+    cin >> cakePrice;
+
+    double chocolatePricePerKg = chocolatePrice / chocolateKilos;
+    double cakePricePerKg = cakePrice / cakeKilos;
+    int priceDifference = cakePricePerKg / chocolatePricePerKg;
+
+    cout.precision(3);
+
+    cout << "Chocolate costs " << chocolatePricePerKg
+	<< " BGN per kilogram." << endl;
+
+	cout << "Cake costs " << cakePricePerKg
+	<< " BGN per kilogram." << endl;
+
+    cout << "Cake is " << priceDifference
+	<< " times more expensive than chocolate.";
+}
