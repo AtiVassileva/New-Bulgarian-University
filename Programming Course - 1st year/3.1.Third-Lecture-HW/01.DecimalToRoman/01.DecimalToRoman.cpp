998b6f4d0635d6f24488eeaf0b609d9cd3ef2e4c
Ati Vassileva <ativassileva@gmail.com>
2021-10-20 11:33:54 +0300

    Solved 1st problem

---
+++ Programming Course - 1st year/3.1.Third-Lecture-HW/01.DecimalToRoman/01.DecimalToRoman.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int number;
+	cout << "Please enter a number between 1 and 4000: ";
+	cin >> number;
+
+	string romanEquivalents[] = { "M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I" };
+
+	int decimalNumbers[] = { 1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1 };
+
+
+	for (int i = 0; i < 13; ++i)
+	{
+		while (number - decimalNumbers[i] >= 0)
+		{
+			cout << romanEquivalents[i];
+			number -= decimalNumbers[i];
+		}
+	}
+}
