1d5719ac7841e0fbb22c0a6651a9e6e47da4bfa3
Ati Vassileva <ativassileva@gmail.com>
2021-10-23 12:49:51 +0300

    Solved 3rd & 4th problem - calculating distance between 2 points

---
+++ Programming Course - 1st year/3.1.Third-Lecture-HW/04.DistanceBetweenTwoPoints-Page105/04.DistanceBetweenTwoPoints-Page105.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+
+using namespace std;
+
+double calculateDistance(int x1, int x2, int y1, int y2)
+{
+    return sqrt(pow(x2 - x1, 2) + pow(y2 - y1, 2));
+}
+
+int main()
+{
+    int x, y;
+
+    cout << "X: ";
+    cin >> x;
+
+    cout << "Y: ";
+    cin >> y;
+
+    cout << "Distance from point(0, 0): " << calculateDistance(x, 0, y, 0) << endl;
+
+    cout << "Distance from point(1, 0): " << calculateDistance(x, 1, y, 0) << endl;
+
+    cout << "Distance from point(0, 1): " << calculateDistance(x, 0, y, 1) << endl;
+
+    cout << "---------------------------" << endl;
+
+    cout << "Distance from point(0, 0): " << calculateDistance(x, 0, y, 0) << endl;
+
+    cout << "Distance from point(1, 0): " << calculateDistance(x, 1, y, 0) << endl;
+
+    cout << "Distance from point(1, 1): " << calculateDistance(x, 1, y, 1) << endl;
+
+    cout << "---------------------------" << endl;
+
+    cout << "Distance from point(-1, 0): " << calculateDistance(x, -1, y, 0) << endl;
+
+    cout << "Distance from point(0, 0): " << calculateDistance(x, 0, y, 0) << endl;
+
+    cout << "Distance from point(1, 1): " << calculateDistance(x, 1, y, 1) << endl;
+}
