3de68f210673d3ffe6a4e8450c6f3fc3f5d16526
Ati Vassileva <ativassileva@gmail.com>
2021-11-04 12:14:34 +0200

    Solved 3rd condition from problem 21 - page 108

---
+++ Programming Course - 1st year/4.1.Fourth-Lecture-HW/21.3.ValidInvalidIdentities-Page108/21.3.ValidInvalidIdentities-Page108.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	unsigned n;
+	cout << "n: ";
+	cin >> n;
+
+	double actualResult = 0;
+
+	for (unsigned i = 0; i <= n; i++)
+	{
+		actualResult += pow(2 * i - 1, 3);
+	}
+
+	double expectedResult = pow(n, 2) * (2 * pow(n, 2) - 1);
+
+	actualResult == expectedResult
+		? cout << "Valid"
+		: cout << "Invalid";
+}
