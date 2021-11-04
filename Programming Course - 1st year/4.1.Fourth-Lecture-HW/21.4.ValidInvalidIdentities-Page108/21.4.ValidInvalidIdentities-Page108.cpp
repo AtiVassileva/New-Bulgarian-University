d2bcbe8a64b1531df87367f41efd0ef6b15c0b75
Ati Vassileva <ativassileva@gmail.com>
2021-11-04 12:33:35 +0200

    Solved 4th condition from problem 21 - page 108

--- Programming Course - 1st year/4.1.Fourth-Lecture-HW/21.4.ValidInvalidIdentities-Page108/21.4.ValidInvalidIdentities-Page108.cpp
+++ Programming Course - 1st year/4.1.Fourth-Lecture-HW/21.4.ValidInvalidIdentities-Page108/21.4.ValidInvalidIdentities-Page108.cpp
@@ -10,12 +10,13 @@ int main()
 
 	double actualResult = 0;
 
-	for (unsigned i = 1; i <= 2 * n - 1; i += 2)
+	for (unsigned i = 0; i <= n; i++)
 	{
-		actualResult += pow(i, 2);
+		actualResult += i * pow(i + 1, 2);
 	}
 
-	double expectedResult = (n * 1.00 / 3) * (4 * pow(n, 2) - 1);
+	double expectedResult = (n * 1.00 / 12) * (n + 1) * (n + 2)
+	* (3 * n + 5);
 
 	actualResult == expectedResult
 		? cout << "Valid"
