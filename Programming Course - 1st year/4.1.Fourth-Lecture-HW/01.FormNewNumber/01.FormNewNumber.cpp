a488af31ff1ce2ce3efddd51b22e99fe28faaef5
Ati Vassileva <ativassileva@gmail.com>
2021-11-06 23:34:01 +0200

    Solved form new number problem

--- Programming Course - 1st year/4.1.Fourth-Lecture-HW/01.FormNewNumber/01.FormNewNumber.cpp
+++ Programming Course - 1st year/4.1.Fourth-Lecture-HW/01.FormNewNumber/01.FormNewNumber.cpp
@@ -4,27 +4,25 @@ using namespace std;
 
 int main()
 {
-	string number;
-	cout << "Number: ";
-	cin >> number;
-
-	string result;
-
-	for (unsigned i = 0; i <= number.length() - 1; i++) 
-	{
-		if (i == 0)
-		{
-			result[number.length() - 1] = number[i];
-		}
-		else if (i == number.length() - 1)
-		{
-			result[0] = number[i];
-		}
-		else
-		{
-			result += number[i + 1];
-		}
-	}
-
-	cout << "Result: " << result;
+    long long n;
+    cin >> n;
+    long long nCopy = n;
+    int length = 0;
+    int digits[1000];
+    while (nCopy != 0)
+    {
+        digits[length] = nCopy % 10;
+        nCopy /= 10;
+        length++;
+    }
+    for (int i = length - 1; i >= 1; i -= 2)
+    {
+        cout << digits[i - 1];
+        cout << digits[i];
+    }
+    if (length % 2 == 1)
+    {
+        cout << digits[0];
+    }
+    cout << endl;
 }
