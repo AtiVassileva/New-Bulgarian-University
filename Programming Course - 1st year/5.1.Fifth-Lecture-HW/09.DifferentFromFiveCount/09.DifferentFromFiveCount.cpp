439b1aab544b60f5fe1f9277ba8fafdf105b778d
Ati Vassileva <ativassileva@gmail.com>
2021-11-11 15:41:31 +0200

    Solved 9th problem

---
+++ Programming Course - 1st year/5.1.Fifth-Lecture-HW/09.DifferentFromFiveCount/09.DifferentFromFiveCount.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int n;
+	cin >> n;
+
+	while (n < 1 || n > 25)
+	{
+		cout << "Numbers count should be between 1 and 25. Try again: ";
+		cin >> n;
+	}
+
+	const auto numbers = new int[n];
+
+	for (int i = 0; i < n; i++)
+	{
+		cin >> numbers[i];
+	}
+
+	int differentNumbersCounter = 0;
+
+	for (int i = 0; i < n; i++)
+	{
+		if (numbers[i] != 5)
+		{
+			differentNumbersCounter++;
+		}
+	}
+
+	differentNumbersCounter > 0
+		? cout << differentNumbersCounter << endl
+		: cout << "NO" << endl;
+
+	delete[] numbers;
+}
