838b21c61724ad3a688fc0e17349fb261cd3c53f
Ati Vassileva <ativassileva@gmail.com>
2021-11-25 10:30:41 +0200

    Solved 3rd problem

--- Programming Course - 1st year/7.2.Exam-Preparation/03.Equalities/03.Equalities.cpp
+++ Programming Course - 1st year/7.2.Exam-Preparation/03.Equalities/03.Equalities.cpp
@@ -1,20 +1,51 @@
-// 03.Equalities.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
+
+using namespace std;
 
 int main()
 {
-    std::cout << "Hello World!\n";
+	int k;
+
+	cout << "k: ";
+	cin >> k;
+
+	if (!cin)
+	{
+		cout << "Bad input!";
+		return 1;
+	}
+
+	if (k <= 0 || k > 40)
+	{
+		cout << "K should be between 1 and 40!";
+		return 2;
+	}
+
+	auto a = new int[k];
+	auto b = new int[k];
+
+	for (int i = 0; i < k; i++)
+	{
+		cout << "a[" << i << "] = ";
+		cin >> a[i];
+
+		cout << "b[" << i << "] = ";
+		cin >> b[i];
+	}
+
+	int counter = 0;
+
+	for (int i = 0; i < k; i++)
+	{
+		for (int j = 0; j < k; j++)
+		{
+			if (a[i] == b[j])
+			{
+				cout << a[i] << " = " << b[j] << endl;
+				counter++;
+			}
+		}
+	}
+
+	cout << "Total equalities: " << counter;
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
