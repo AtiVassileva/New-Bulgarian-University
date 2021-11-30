ff0c646e6ec9f112c5a7a1f08916dfaa31916491
Ati Vassileva <ativassileva@gmail.com>
2021-12-01 00:17:41 +0200

    Partly solved 3rd problem. Needs refactoring.

---
+++ 7.1.Seventh-Lecture-HW/03.NumbersInText/03.NumbersInText.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+    string text;
+
+    cout << "Enter text:" << endl;
+    getline(cin, text);
+
+    int arrayLength = 0;
+
+    for (const char symbol : text)
+    {
+	    if (isdigit(symbol))
+	    {
+            arrayLength++;
+	    }
+    }
+
+    const auto numbers = new int[arrayLength];
+
+    int position = 0;
+
+    for (const char symbol : text)
+    {
+        if (isdigit(symbol))
+        {
+            numbers[position] = (int)symbol;
+            position++;
+        }
+    }
+
+	for (int i = 0; i < arrayLength; i++)
+    {
+        cout << numbers[i] << " ";
+    }
+
+    delete[] numbers;
+}
