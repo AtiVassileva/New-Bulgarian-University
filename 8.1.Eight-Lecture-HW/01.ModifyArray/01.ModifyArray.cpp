b34d9533b862fe5f1c4da4bba1c345c8ee0f0735
Ati Vassileva <ativassileva@gmail.com>
2021-12-21 10:48:09 +0200

    Solved 1st problem

--- 8.1.Eight-Lecture-HW/01.ModifyArray/01.ModifyArray.cpp
+++ 8.1.Eight-Lecture-HW/01.ModifyArray/01.ModifyArray.cpp
@@ -2,10 +2,8 @@
 
 using namespace std;
 
-void printArray(const int* array)
+void printArray(const int* array, const int length)
 {
-    constexpr unsigned length = sizeof(array);
-
     for (int i = 0; i < length; i++)
     {
         cout << array[i] << " ";
@@ -14,14 +12,14 @@ void printArray(const int* array)
     cout << endl;
 }
 
-void deleteElement(int arr[], const int length, const int number)
+void deleteElement(int array[], const int length, const int number)
 {
     bool containsNumber = false;
     int i;
 
     for (i = 0; i < length; i++)
     {
-        if (arr[i] == number)
+        if (array[i] == number)
         {
             containsNumber = true;
             break;
@@ -37,7 +35,7 @@ void deleteElement(int arr[], const int length, const int number)
     {
         for (int j = i; j < length - 1; j++)
         {
-            arr[j] = arr[j + 1];
+            array[j] = array[j + 1];
         }
     }
 }
@@ -55,18 +53,89 @@ auto insertAtTheEnd(const int* array, const int length, int number)
     arrayCopy[copyLength - 1] = number;
 
     return arrayCopy;
-   // array = arrayCopy;
+}
+
+auto insertAtIndex(const int* array, const int length, 
+    const int number, const int index)
+{
+    const int copyLength = length + 1;
+    const auto arrayCopy = new int[copyLength];
+
+    for (int i = 0; i < copyLength; i++)
+    {
+	    if (i == index)
+	    {
+            arrayCopy[i] = number;
+
+            for (int j = i; j < length; j++)
+            {
+                arrayCopy[j + 1] = array[j];
+            }
+
+            break;
+	    }
+
+	    arrayCopy[i] = array[i];
+    }
+
+    return arrayCopy;
+}
+
+void changeValue (int* array, const int length, 
+    const int oldValue, const int newValue)
+{
+    bool containsNumber = false;
+    int i;
+
+    for (i = 0; i < length; i++)
+    {
+        if (array[i] == oldValue)
+        {
+            containsNumber = true;
+            break;
+        }
+    }
+
+    if (!containsNumber)
+    {
+        return;
+    }
+
+    array[i] = newValue;
 }
 
 int main()
 {
     const auto deleteArray = new int[5]{ 5, 6, 7, 8, 9 };
-    deleteElement(deleteArray, sizeof(deleteArray), 5); 
+    constexpr unsigned deleteArrayLength = 5;
+    deleteElement(deleteArray, deleteArrayLength, 7); 
 
-    printArray(deleteArray); // 1, 2, 3, 4
+    printArray(deleteArray, deleteArrayLength - 1);
+	// 5, 6, 8, 9
 
-    const auto insertArray = new int[3]{ 17, 18, 19 };
-    auto result = insertAtTheEnd(insertArray, 3, 20);
+    const auto insertAtEndArray = new int[3]{ 17, 18, 19 };
+    constexpr unsigned insertAtEndArrayLength = 3;
+    const auto insertAtEndResult = insertAtTheEnd(insertAtEndArray, insertAtEndArrayLength, 20);
     
-    printArray(result);
+    printArray(insertAtEndResult, insertAtEndArrayLength + 1);
+	// 17, 18, 19, 20
+
+    const auto insertArray = new int[4]{ 22, 16, 78, 92 };
+    constexpr unsigned insertArrayLength = 4;
+    const auto insertResult = insertAtIndex(insertArray, insertArrayLength, 50, 3);
+
+    printArray(insertResult, insertArrayLength + 1);
+	// 22, 16, 78, 50, 92
+
+    const auto changeValueArray = new int[6]{ 9, 3, 2, 8, 7, 5 };
+    constexpr unsigned changeValueArrayLength = 6;
+    changeValue(changeValueArray, changeValueArrayLength, 8, 99);
+
+    printArray(changeValueArray, changeValueArrayLength);
+    // 9, 3, 2, 99, 7, 5
+
+    delete[] deleteArray;
+    delete[] insertAtEndArray;
+    delete[] insertArray;
+    delete[] changeValueArray;
 }
