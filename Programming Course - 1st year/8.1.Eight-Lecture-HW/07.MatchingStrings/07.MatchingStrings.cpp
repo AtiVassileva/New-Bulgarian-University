883d1131a8de77c8988f90e54dd67814bbfa5f42
Ati Vassileva <ativassileva@gmail.com>
2021-12-23 10:57:09 +0200

    Solved 7th problem

---
+++ Programming Course - 1st year/8.1.Eight-Lecture-HW/07.MatchingStrings/07.MatchingStrings.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int countMatchingWords(const string& firstString, 
+	const string& secondString)
+{
+	int count = 0;
+
+	string currentWord;
+
+	for (const auto symbol : firstString)
+	{
+		if (symbol == ' ')
+		{
+			if (secondString.find(currentWord) != string::npos)
+			{
+				count++;
+			}
+
+			currentWord = "";
+		}
+		else 
+		{
+			currentWord += symbol;
+		}
+	}
+
+	return count;
+}
+
+int main()
+{
+    string firstString;
+    cout << "First string: ";
+    getline(cin, firstString);
+
+    string secondString;
+    cout << "Second string: ";
+    getline(cin, secondString);
+
+    const auto count = countMatchingWords(firstString, secondString);
+
+    cout << "Matching words: " << count << endl;
+}
