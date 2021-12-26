d08842b8484141e621f9fe09fabe447079f6e78a
Ati Vassileva <ativassileva@gmail.com>
2021-12-26 20:21:15 +0200

    Solved 4th problem

--- Programming Course - 1st year/8.1.Eight-Lecture-HW/04.FillMatrix/04.FillMatrix.cpp
+++ Programming Course - 1st year/8.1.Eight-Lecture-HW/04.FillMatrix/04.FillMatrix.cpp
@@ -1,20 +1,61 @@
-// 04.FillMatrix.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
+
+using namespace std;
+
+int** fillMatrix(int** matrix, int rows, int cols)
+{
+	for (int row = 0; row < rows; row++)
+	{
+        for (int col = 0; col < cols; col++)
+        {
+            int number;
+            cout << "matrix[" << row << "][" << col << "] = ";
+            cin >> number;
+
+            if (row % 2 == 0)
+            {
+                matrix[row][col] = number;
+            }
+            else
+            {
+                matrix[row][cols - col - 1] = number;
+            }
+        }
+	}
+
+    return matrix;
+}
+
+void printMatrix(int** matrix, int rows, int cols)
+{
+	for (int row = 0; row < rows; row++)
+	{
+		for (int col = 0; col < cols; col++)
+		{
+            cout << matrix[row][col] << " ";
+		}
+
+        cout << endl;
+	}
+}
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    int rows, cols;
+
+    cout << "Rows: ";
+    cin >> rows;
+
+    cout << "Columns: ";
+    cin >> cols;
+
+    auto matrix = new int* [rows];
+
+    for (int i = 0; i < cols; i++)
+    {
+        matrix[i] = new int[cols];
+    }
+
+    matrix = fillMatrix(matrix, rows, cols);
+    printMatrix(matrix, rows, cols);
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
