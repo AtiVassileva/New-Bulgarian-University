0bad49e35b1d1365583cc7e87e716be97b4777a2
Ati Vassileva <ativassileva@gmail.com>
2022-01-11 09:51:27 +0200

    Solved 1st problem

---
+++ Labs-1st year/Problem01/Problem01.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+
+using namespace std;
+
+void printArray(int* array, int length);
+int setPower(char*, int size);
+
+int main()
+{
+	const int size = 5;
+	int array[size] = { 1, 2, 3, 4, 5 };
+	char bitMask [] = "10110";
+
+	cout << "Set power: " << setPower(bitMask, size);
+}
+
+void printArray(int* array, int length) {
+	for  (int i = 0; i < length; i++)
+	{
+		cout << array[i] << " ";
+	}
+
+	cout << endl;
+}
+
+int setPower(char* set, int size) {
+
+	int counter = 0;
+
+	for (int i = 0; i < size; i++)
+	{
+		if (set[i] == '1')
+		{
+			counter++;
+		}
+	}
+
+	return counter;
+}
