f14496e444fc0e283cabaf4c7786a71a49d8c59a
Ati Vassileva <ativassileva@gmail.com>
2022-01-13 10:29:26 +0200

    Solved 2nd problem

--- Programming Course - 1st year/13.0.ExamPreparation/02.ReverseArray/02.ReverseArray.cpp
+++ Programming Course - 1st year/13.0.ExamPreparation/02.ReverseArray/02.ReverseArray.cpp
@@ -4,8 +4,57 @@ using namespace std;
 
 int* fillArray(int[], int);
 void printArray(int[], int);
+int* reverseArray(int[], int);
 
 int main()
 {
-    
+	int length;
+	cout << "Array length: ";
+	cin >> length;
+
+	auto array = new int[length];
+
+	fillArray(array, length);
+	printArray(array, length);
+
+	auto result = reverseArray(array, length);
+	printArray(result, length);
+}
+
+int* fillArray(int array[], int length) 
+{
+	for (int i = 0; i < length; i++)
+	{
+		array[i] = rand();
+	}
+
+	return array;
+}
+
+void printArray(int array[], int length)
+{
+	for (int i = 0; i < length; i++)
+	{
+		cout << array[i] << " ";
+	}
+	cout << endl;
+}
+
+int* reverseArray(int array[], int length)
+{
+	auto result = new int[length]{0};
+
+	int start = 0;
+	int end = length - 1;
+
+	while (start < length)
+	{
+		int temp = array[start];
+		result[start] = array[end];
+		result[end] = temp;
+		start++;
+		end--;
+	}
+
+	return result;
 }
